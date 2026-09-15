ee9aca924af47cf100de1eeeb8d82addafc12209
Aluno <e023aluno@outlook.com>
2026-09-14 21:54:47 -0300

    Adiciona node_modules ao gitignore

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() { 
+  return ( 
+    <main className="min-h-screen bg-slate-100 p-8"> 
+      <h1 className="text-4xl font-bold text-yellow-500"> 
+        React + Tailwind CSS 
+      </h1> 
+ 
+      <button className=" 
+        mt-6 rounded-lg bg-black px-4 py-2 
+        font-semibold text-yellow-400 
+        hover:bg-slate-800 
+      "> 
+        Testar Tailwind 
+      </button> 
+    </main> 
+  ) 
+} 
