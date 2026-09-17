05a3eea457c2bed2c0c69b042ef12a4a30271bd6
Aluno <e023aluno@outlook.com>
2026-09-17 20:26:44 -0300

    feat: alteraçoes dos componentes

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,17 +1,19 @@
-export default function App() { 
-  return ( 
-    <main className="min-h-screen bg-slate-100 p-8"> 
-      <h1 className="text-4xl font-bold text-yellow-500"> 
-        React + Tailwind CSS 
-      </h1> 
- 
-      <button className=" 
-        mt-6 rounded-lg bg-black px-4 py-2 
-        font-semibold text-yellow-400 
-        hover:bg-slate-800 
-      "> 
-        Testar Tailwind 
-      </button> 
-    </main> 
-  ) 
-} 
+import Header from './components/Header'
+
+
+export default function App(){
+  return(
+    <div className='min-h-screen bg-slate-100 text-slate-900'>
+
+      <header/>
+
+      <main>
+        {/*<Hero/>
+        <recursos/>
+        <sobre/> */ }
+      </main>
+
+      {/* <footer/> */}
+    </div>
+  )
+}
